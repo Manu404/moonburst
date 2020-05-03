2a569c843d09382dd45bb59b2401e1d9239835b8
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2020-05-03 09:02:15 +0200

    fix som build config issue, output builds to /Output/<arch>/<config>/

--- MoonBurst.Client/Properties/AssemblyInfo.cs
+++ MoonBurst.Client/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.2.0.2013")]
-[assembly: AssemblyFileVersion("2020.2.0.2011")]
+[assembly: AssemblyVersion("2020.2.0.2017")]
+[assembly: AssemblyFileVersion("2020.2.0.2015")]
