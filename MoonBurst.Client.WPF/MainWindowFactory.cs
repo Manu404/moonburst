bdccfd61ac1f7d92e3accbed5fd90130b8b65667
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2020-05-04 10:09:56 +0200

    upate build output and app icon

---
+++ MoonBurst.Client.WPF/MainWindowFactory.cs
@@ -0,0 +1,22 @@
+﻿using MoonBurst.Core;
+using MoonBurst.View;
+using MoonBurst.ViewModel.Interface;
+
+namespace MoonBurst
+{
+    public class MainWindowFactory : IMainWindowFactory
+    {
+        private IMainViewModel mainViewModel;
+        private IFactory<IMainView> viewFactory;
+
+        public MainWindowFactory(IMainViewModel mainViewModel, IFactory<IMainView> viewFactory)
+        {
+            this.mainViewModel = mainViewModel;
+            this.viewFactory = viewFactory;
+        }
+        public IMainViewHost Build()
+        {
+            return new MainWindow(mainViewModel, viewFactory);
+        }
+    }
+}
--- MoonBurst.Client/MainWindow.xaml.cs
+++ MoonBurst.Client/MainWindow.xaml.cs
@@ -1,25 +1,8 @@
 ﻿using MoonBurst.Core;
-using MoonBurst.View;
 using MoonBurst.ViewModel.Interface;
 
 namespace MoonBurst
 {
-    public class MainWindowFactory : IMainWindowFactory
-    {
-        private IMainViewModel mainViewModel;
-        private IFactory<IMainView> viewFactory;
-
-        public MainWindowFactory(IMainViewModel mainViewModel, IFactory<IMainView> viewFactory)
-        {
-            this.mainViewModel = mainViewModel;
-            this.viewFactory = viewFactory;
-        }
-        public IMainViewHost Build()
-        {
-            return new MainWindow(mainViewModel, viewFactory);
-        }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
