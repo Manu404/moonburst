acdfe620690d5441fc1269421996438d3ebae8c3
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2020-08-05 16:37:45 +0200

    check pre-existing instance for standalone hosting.

---
+++ MoonBurst.Core/InstanceHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace MoonBurst.Core
+{
+    public class InstanceHelper
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private Process _existingProcess;
+
+        public bool SetExstingProcessForegroundWindow()
+        {
+            if (_existingProcess == null) return false;
+            return SetForegroundWindow(_existingProcess.MainWindowHandle);
+        }
+
+        public bool IsSingleInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            string currentmd5 = getFileMd5(current.MainModule.FileName);
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process process in processlist)
+            {
+                if (process.Id != current.Id)
+                {
+                    try
+                    {
+                        if (currentmd5 == getFileMd5(process.MainModule.FileName))
+                        {
+                            _existingProcess = process;
+                            return false;
+                        }
+                    }
+                    catch { }
+                }
+            }
+            return _existingProcess == null;
+        }
+
+        private string getFileMd5(string file)
+        {
+            string check;
+            using (FileStream FileCheck = File.OpenRead(file))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] md5Hash = md5.ComputeHash(FileCheck);
+                check = BitConverter.ToString(md5Hash).Replace("-", "").ToLower();
+            }
+
+            return check;
+        }
+    }
+}
--- MoonBurst.Standalone/EntryPoint.cs
+++ MoonBurst.Standalone/EntryPoint.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using Castle.MicroKernel.Registration;
 using MoonBurst.Api.Client;
+using MoonBurst.Core;
 
 namespace MoonBurst
 {
-
     public class EntryPoint
     {
         [STAThread()]
         public static void Main(string[] args)
         {
+            /*var instanceHelper = new InstanceHelper();
+            if (!instanceHelper.IsSingleInstance())
+               if(instanceHelper.SetExstingProcessForegroundWindow())
+                    Environment.Exit(0);            
+            */
+
             var boot = Bootstrapper.GetDefaultContainer();
             boot.Register(Component.For<IMainViewHostFactory>().ImplementedBy<WindowMainViewHostFactory>());
             boot.Register(Component.For<IStartupOptionParser>().Instance(new CommandLineStartOptionParser(args)));
